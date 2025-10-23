e92c1fa378fa4b8fd695d58e5b66877bc3da1b0e
Jan Prskavec <jan.prskavec@tul.cz>
2025-10-23 13:40:58 +0200

    rozpracovane nejvetsi ze tri cisel

---
+++ IS-programy/program003b-serazeni-tri-cisel/Program.cs
@@ -0,0 +1,55 @@
+﻿﻿using System.Runtime.InteropServices.Marshalling;
+
+
+string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("********************************************");
+    Console.WriteLine("*********** Seřazení dvou čísel*************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("************* Jan Prskavec *****************");
+    Console.WriteLine("************** 23.10.2025 ******************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine();
+
+
+    Console.Write("Zadejte celé číslo A: ");
+    int a;
+    while (!int.TryParse(Console.ReadLine(), out a))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte celé číslo znovu A: ");
+    }
+
+    Console.Write("Zadejte celé číslo B: ");
+    int b;
+    while (!int.TryParse(Console.ReadLine(), out b))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte celé číslo znovu B: ");
+    }
+
+    Console.Write("Zadejte celé číslo C: ");
+    int b;
+    while (!int.TryParse(Console.ReadLine(), out c))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte celé číslo znovu C: ");
+    }
+
+    Console.WriteLine();
+
+    if (a > b) ;
+        if (a > c)
+        
+    
+    
+    
+    Console.WriteLine();
+    Console.WriteLine("==============================================================");
+    Console.WriteLine($"Seřazená čísla: {a}, {b}");
+    Console.WriteLine("==============================================================");
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+}
