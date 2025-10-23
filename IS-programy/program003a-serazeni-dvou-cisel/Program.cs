e3c8db6148eeaf5dc258c6d0ab609cc31c3c6d4a
Jan Prskavec <jan.prskavec@tul.cz>
2025-10-23 13:10:20 +0200

    opakování programu

---
+++ IS-programy/program003a-serazeni-dvou-cisel/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Runtime.InteropServices.Marshalling;
+
+string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("********************************************");
+    Console.WriteLine("*********** Seřazení dvou čísel*************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("************* Jan Prskavec *****************");
+    Console.WriteLine("************** 23.10.2025 ******************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine();
+
+
+    Console.Write("Zadejte celé číslo A: ");
+    int a;
+    while (!int.TryParse(Console.ReadLine(), out a))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte první číslo řady znovu: ");
+    }
+
+
+    Console.Write("Zadejte celé číslo B: ");
+    int b;
+    while (!int.TryParse(Console.ReadLine(), out b))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte poslední číslo řady znovu: ");
+    }
+
+    Console.WriteLine();
+
+    //chceme seradit cisla vzestupne
+    if (a > b)
+    {
+
+        pom = a;
+        a = b;
+        b = pom
+
+    }
+
+    Console.WriteLine("==========================================");
+    Console.WriteLine($"Seřazená čísla? {a}, {b}");
+    Console.WriteLine("==========================================");
+    Console.WriteLine();
+    Console.WriteLine("Pro okakování programu stiskněte klávesu a");
+      
+    
+
+    
+    
+
+
+
+
+
+
+
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    Console.ReadLine( );
+
+}
